a0ecccd54000d940d4a5b0cd0077eb586bdb5fbc
fredlc <fredlefevre@outlook.com>
2019-06-24 12:49:37 -0400

    Integrated search users functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Navbar from "./components/layout/Navbar";
+import Search from "./components/users/Search";
 import Users from "./components/users/Users";
 import "./App.css";
 
@@ -10,21 +11,32 @@ class App extends React.Component {
     loading: false
   };
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  searchUsers = async text => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${
+      `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
